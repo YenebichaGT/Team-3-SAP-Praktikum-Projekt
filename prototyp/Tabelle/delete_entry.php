ed7e0dd0c367e9231808cae3117d922c5c7882e4
MetinInl <metin.erdem@manos-dresden.lernsax.de>
2026-02-19 09:22:59 +0100

    Tabelle mit Einträgen

---
+++ prototyp/Tabelle/delete_entry.php
@@ -0,0 +1,35 @@
+<?php
+// delete_entry.php
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo json_encode(['error' => 'Nur POST erlaubt']);
+    exit;
+}
+
+$input = json_decode(file_get_contents('php://input'), true);
+if (!isset($input['index'])) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Kein Index angegeben']);
+    exit;
+}
+
+$dataFile = 'data.json';
+if (!file_exists($dataFile)) {
+    http_response_code(404);
+    echo json_encode(['error' => 'Datei nicht gefunden']);
+    exit;
+}
+
+$data = json_decode(file_get_contents($dataFile), true);
+if (!isset($data['elements'][$input['index']])) {
+    http_response_code(404);
+    echo json_encode(['error' => 'Eintrag nicht gefunden']);
+    exit;
+}
+
+array_splice($data['elements'], $input['index'], 1);
+file_put_contents($dataFile, json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+echo json_encode(['success' => true]);
